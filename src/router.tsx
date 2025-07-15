b26fd9167d974f6aea45764728b3d67050c449f9
Bruno Rocha <brunorocha2674@gmail.com>
2025-07-15 12:52:37 -0300

    feat: add access logger

--- src/router.tsx
+++ src/router.tsx
@@ -1,9 +1,12 @@
+import { useDailyAppPing } from '@/hooks/use-daily-app-ping'
 import { CartPage } from '@/pages/cart'
 import { HomePage } from '@/pages/home'
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router'
 import { ScrollToTop } from './scrool-to-top'
 
 export function Router(): React.JSX.Element {
+  useDailyAppPing()
+
   return (
     <BrowserRouter>
       <ScrollToTop>
